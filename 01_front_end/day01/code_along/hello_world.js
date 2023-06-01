332da4fca5c62fb24335608a7cea90c34a960c8d
bharathp9 <bharath@akademiga.com>
2023-06-01 17:32:38 +0800

    updated day01 code along

---
+++ 01_front_end/day01/code_along/hello_world.js
@@ -0,0 +1,71 @@
+//Single Line Comment
+//console.log('Hello Every one')
+/* Multi-line comment
+console.log('Hello Every one')
+console.log('Hello Every one')
+console.log('Hello Every one')
+console.log('Hello Every one')
+console.log('Hello Every one')
+*/
+// console.log('Hello Every one')
+// console.log('Hello Every one')
+// console.log('Hello Every one')
+
+//Declare a variable
+// var phrase = "Hello Welcome to SEI";
+
+// let my_name = "Bharath"
+
+// console.log('Hello '+ my_name);
+
+// my_name = "Bharath Kumar"
+
+// //Print the variable message
+// console.log(phrase);
+
+// console.log('Hello '+ my_name);
+
+
+// //Const variable
+// const sumNum = 99 +1;
+// // sum_num = sum_num+1
+// console.log(sumNum)
+
+// let a = '';
+// let b = 'bongos';
+// let c = 'get your';
+
+// a = b;
+// b = c; 
+// c = a;
+
+// console.log(a+b+c);
+
+// //Print message five times
+// console.log('Hello Every one')
+// console.log('Hello Every one')
+// console.log('Hello Every one')
+// console.log('Hello Every one')
+// console.log('Hello Every one')
+
+// //While executes until given statement is false
+// let num = 10;
+// while (num >= 1){
+//     console.log("Back to home: "+num);
+//     num--;
+// }
+
+//Comparative Operators
+/*
+> greater than
+< less than
+== equal to
+>= greater than or equal to
+<= less than or equal to
+!= not equal
+*/
+
+//for loop exexutes given number of times:
+for(let i=0; i<=10; i++){
+    console.log(i)
+}
---
+++ 01_front_end/day01/student_notes/README.md
@@ -0,0 +1,10 @@
+# Java Script
+
+- High Level Programming Language
+- Design/Develop interactive web content
+- Most popuplar programming language
+
+# Why Javascript
+
+- Can use every where (front end and backend)
+- 

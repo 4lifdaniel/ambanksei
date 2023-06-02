99ec3900a9f34dfda8c90f2dc558cd19a2f5512e
bharathp9 <bharath@akademiga.com>
2023-06-02 17:51:05 +0800

    updated day 02 code along

---
+++ 01_front_end/day02/code_along/conditional_practice.js
@@ -0,0 +1,86 @@
+/*
+if (Boolean Expression){
+    //run this code
+}
+*/
+
+// Boolean allows us to set true or false values
+
+// let myFavColor = 'green';
+// if (myFavColor == 'blue'){
+//     console.log("Mine as well");
+// } else {
+//     console.log("Your fav color is: "+myFavColor);
+// }
+
+// let isPartyTonight = false;
+// let isShoppingTime = false;
+// let isSleepTime = true;
+// let isGoingHomeTime = true;
+// let isClassTime = true;
+
+// if (isPartyTonight) {
+//   console.log("What do i need to wear");
+// } else if (isShoppingTime) {
+//   console.log("Got any sale?");
+// } else if (isSleepTime) {
+//   console.log("Put on Pyjamas");
+// }
+// if (!isGoingHomeTime) {
+//   console.log("Pack your bags");
+// } else {
+//   console.log("Please concentrate");
+// }
+
+// console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Hi"));
+// console.log(Boolean(1));
+
+// let a = "1";
+// let b = 2;
+
+// if (a !== b) {
+//   console.log("These are different values");
+// } else {
+//   console.log("They are same");
+// }
+
+// if (a === b) {
+//   console.log("These are same values");
+// } else {
+//   console.log("They are not same");
+// }
+
+// console.log(5 + 5 * 3);
+// console.log((5 + 5) * 3);
+
+// const a = true;
+// const b = true;
+// const c = false;
+
+// console.log(a && b); //true
+// console.log(a && c); //false
+// console.log(c && c); //false
+// console.log(a || c); //true
+// console.log(a || b); //true
+
+// const username = "Bharath";
+// // const username = "";
+// let user = username || "bob bob sleepy bob";
+
+// console.log(user);
+
+// console.log(!false && true); //true
+
+//Modulo operator
+
+if (5 % 2 === 0) {
+  console.log("Its even number");
+}
+
+const redColor = "red";
+console.log(redColor);
---
+++ 01_front_end/day02/code_along/math.js
@@ -0,0 +1,30 @@
+// let cartTotal = 16.07;
+// let roundedCartTotal = Math.round(cartTotal);
+// console.log(roundedCartTotal);
+
+// let roundCeil = Math.ceil(cartTotal);
+// console.log(roundCeil);
+
+
+// let donation = (roundCeil - cartTotal).toFixed(2);
+// console.log(donation);
+
+// console.log(Math.abs(-5));
+// console.log(Math.abs(5.0));
+
+// console.log(Math.PI);
+
+// console.log(Math.ceil(3.2));
+// console.log(Math.ceil(3.8));
+
+// console.log(Math.max(2,8,5));
+// console.log(Math.max(-2,-8,-5));
+
+// console.log(Math.min(2,8,5));
+// console.log(Math.min(-2,-8,-5));
+
+// console.log(Math.pow(2,3));
+// console.log(Math.pow(5,3));
+
+console.log(Math.floor(3.2));
+

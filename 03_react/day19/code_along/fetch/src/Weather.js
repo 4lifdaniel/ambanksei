6e76bd50768583878e17a8dad79e75058aae0ac8
bharathp9 <bharath@akademiga.com>
2023-07-04 13:50:48 +0800

    day20 starter code

--- 03_react/day19/code_along/fetch/src/Weather.js
+++ 03_react/day19/code_along/fetch/src/Weather.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Weather = () => {
   const APIKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
@@ -6,21 +6,44 @@ const Weather = () => {
 
   //Create a state to store temp
   const [temp, setTemp] = useState(0);
+  //Create a state to store location details
+  const [location, setLocation] = useState("Kuala Lumpur");
+  //Create state to store message
+  const [message, setMessage] = useState("");
+
+  //Will execute fetchWeather on initial load
+  // useEffect(fetchWeather, []);
+
+  //Will execute fetchWeather everytime location updates
+  useEffect(fetchWeather, [location]);
+
+  // console.log(useState("Malcolm"));
+
+  function handleLocation() {
+    setLocation(document.getElementById("Location").value);
+  }
 
   function fetchWeather() {
     //Fetch Weather from API
 
     //Generate the URL for fetch request
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Kuala%20Lumpur&units=metric&appid=${APIKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
 
     //Create a fetch request
     fetch(url)
       .then((response) => {
         //The data is returned as a promise response, so we need to use .then on response.json()
-        response.json().then((jsonData) => {
+        const responseValue = response.json();
+        // console.log(responseValue);
+        responseValue.then((jsonData) => {
           //In the jsonData, the value we need is inside main object with key name  "temp"
           //console.log(jsonData.main.temp);
-          setTemp(jsonData.main.temp);
+          if (jsonData.message) {
+            setMessage(jsonData.message);
+          } else {
+            setMessage("");
+            setTemp(jsonData.main.temp);
+          }
         });
       })
       .catch((error) => {
@@ -29,8 +52,20 @@ const Weather = () => {
   }
   return (
     <div>
-      <button onClick={fetchWeather}>Get Current Temperature</button>
+      <select id="Location" onChange={handleLocation}>
+        <option value="Bangi">Bangi</option>
+        <option value="Kuala Lumpur">Kuala Lumpur</option>
+        <option value="Singapore">Singapore</option>
+        <option value="Jakarta">Jakarta</option>
+        <option value="Bali">Bali</option>
+      </select>
+      <p>Location: {location}</p>
       <p>Temperature: {temp}°C</p>
+      {/* <input type="text" id="Location" onChange={handleLocation} required /> */}
+      {/* <button onClick={fetchWeather}>Get Current Temperature</button>
+      <p>
+        {message ? message : `Location: ${location} and Temperature: ${temp}°C`}
+      </p> */}
     </div>
   );
 };
---
+++ 03_react/day20/homework/README.md.md
@@ -0,0 +1,23 @@
+# React Movie Viewer
+Build a movie details viewer using React and React Router
+
+
+## Instructions
+1. Create a react app in this directory (`movie-viewer`) using the command
+`npx create-react-app .`
+1. Remove any unecessary files.
+1. Install React router using `npm i react-router-dom`.
+1. Move `data.json` into the `src` folder once your React app is scaffolded.
+1. Meet the requirements below.
+
+
+## Requirements
+Using React and React Router, build a simple website showcasing the details for
+all the Star Wars Movies. Your app will have a sidebar, listing all the Star Wars
+movies and linking to a show page in the main section of the page. When a user
+clicks on a movie from the sidebar list, they should see the details for that
+movie rendered in the main content area of the page. Use the data provided in
+`data.json`.
+
+![Homepage](./images/wdi-star-wars-movie-viewer-homepage-default.png)
+![Homepage](./images/wdi-star-wars-movie-viewer-showpage.png)
